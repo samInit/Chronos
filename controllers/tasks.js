1689945b2cbb81359dd9e3862901abeafb31abee
samInit <sameeramadhu9304@gmail.com>
2025-10-19 23:54:53 +0530

    [feat] get all tasks and get a single task controller logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,13 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res) =>{
-    res.send('get all tasks')
+const getAllTasks = async (req,res) =>{
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
+    // res.send('get all tasks')
 }
 
 const createTask = async (req,res) =>{
@@ -14,9 +20,18 @@ const createTask = async (req,res) =>{
     // res.send('create task')
     // res.json(req.body)//for postman testing
 }
-const getTask = (req,res) =>{
+const getTask = async (req,res) =>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg: `No task with ID: ${taskID}`})
+        }
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
     // res.send('get a task')
-    res.json({id:req.params.id})
+    // res.json({id:req.params.id})
 }
 const updateTask = (req,res) =>{
     res.send('update a task')
